289e6fe4b8809e0e62c59fa888db1180e4aa525f
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-10-15 15:57:16 -0400

    added binary rep problem

---
+++ binaryRepresenation.js
@@ -0,0 +1,18 @@
+// Binary Representation
+/*
+What is the input to the program?
+  A number
+What is the output of the program?
+  A binary representation of that number
+What is the input to each recursive call?
+  An instance of anagrams() with a permutation of the remaining string
+What is the output of each recursive call?
+  ''
+
+Write a recursive function that prints out the binary
+representation of a given number. For example, the 
+program should take 3 as an input and print 11 as output, 
+or 25 as an input and print 11001 as an output. Note 
+that the binary representation of 0 should be 0.
+*/
+
