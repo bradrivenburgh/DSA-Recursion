da9f4c74aa3ef51bca5d4aa40fdca0b4a0de4cde
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-10-20 11:45:16 -0400

    solved binary representation

--- binaryRepresenation.js
+++ binaryRepresenation.js
@@ -1,13 +1,14 @@
 // Binary Representation
 /*
 What is the input to the program?
-  A number
+  A decimal number
 What is the output of the program?
-  A binary representation of that number
+  A binary representation of that decimal number
 What is the input to each recursive call?
-  An instance of anagrams() with a permutation of the remaining string
+  num divided by 2 (rounded to nearest int)
 What is the output of each recursive call?
-  ''
+  The product of num % 2 + 10 and the returned value
+  from  the previous instance of binary
 
 Write a recursive function that prints out the binary
 representation of a given number. For example, the 
@@ -16,3 +17,12 @@ or 25 as an input and print 11001 as an output. Note
 that the binary representation of 0 should be 0.
 */
 
+function binary(num) {
+  if (num === 0) {
+    return 0;
+  } else {
+    return num % 2 + 10 * binary(Math.floor(num / 2));
+  }
+}
+
+console.log(binary(17)); //11
